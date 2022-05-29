7a3882fe99a9a1e8ae043240e074ea062d52de56
Kia <98191783+kiakaikia@users.noreply.github.com>
2022-05-28 18:32:29 -0700

    updated works

--- src/data/works.js
+++ src/data/works.js
@@ -6,5 +6,11 @@ export default [
         imgUrl: logo,
         tech: ['JSX', 'HTML', 'React', 'JS'],
         workUrl: 'https://kiakaikia.github.io/'
+    },
+    {
+        title: 'Note Taking',
+        imgUrl: logo,
+        tech: ['HTML', 'CSS', 'JS', 'Flask/Python'],
+        workUrl: 'https://kiakaikia.github.io/'
     }
 ]
