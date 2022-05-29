b8bbdbf2b45b5b43913972ca0703902d04ffbbe5
Kia <98191783+kiakaikia@users.noreply.github.com>
2022-05-28 18:43:47 -0700

    updated 'About Me' Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,9 @@ function About() {
         <div className='w-full md:w-6/12'>
         <SectionTitle>About Me</SectionTitle>
         <p className='text-md text-gray-600 dark:text-gray-300'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Velit ullam laborum ut sint ipsam excepturi!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Velit ullam laborum ut sint ipsam excepturi!
+            Thanks for visiting my webpage! I'm Kiarash Shirazi,
+            a recent 'Computer Science' graduate who's looking to expand his
+            knowledge and work on irl projects. If you have any concern, you can email me anytime!
         </p>
         <a href="mailto:kiaaw.sh@gmail.com"
         className='block mt-3 text-md md:text-lg
