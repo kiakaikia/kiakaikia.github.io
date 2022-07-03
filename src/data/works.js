a407f15cb941e69600a34fc03d67005128cb2d91
Kia <98191783+kiakaikia@users.noreply.github.com>
2022-07-02 19:36:51 -0700

    added a new website

--- src/data/works.js
+++ src/data/works.js
@@ -1,10 +1,12 @@
 import logo from './media/Portfolio.png'
 import logo2 from './media/note_taking.png'
+import logo3 from './media/appointment.png'
+
 export default [
     {
         title: 'Portfolio',
         imgUrl: logo,
-        tech: ['HTML', 'CSS', 'JS/React', 'JSX'],
+        tech: ['HTML', 'CSS', 'JS/React', 'Tailwind'],
         workUrl: 'https://kiakaikia.github.io/'
     },
     {
@@ -12,5 +14,11 @@ export default [
         imgUrl: logo2,
         tech: ['HTML', 'CSS', 'JS', 'Flask/Python'],
         workUrl: 'https://github.com/kiakaikia/Notetaking'
+    },
+    {
+        title: 'Appointment App',
+        imgUrl: logo3,
+        tech: ['HTML', 'CSS', 'JS/React', 'Tailwind'],
+        workUrl: 'https://appoitnmentreact.netlify.app/'
     }
 ]
